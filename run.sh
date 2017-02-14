a423d7f0f95fbfa20ed9e84f702cd9114bcbff1f
Lee Keitel <lfkeitel@usi.edu>
2017-02-14 10:14:23 -0600

    Wait for all containers to start, join existing cluster

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 SERVICE_NAME=${SERVICE_NAME:-etcd}
+CLUSTER_SIZE${CLUSTER_SIZE:-1}
+SERVICE_CONTAINER=""
+NUM_OF_PEERS=0
 
 # Check for $ETCD_LISTEN_CLIENT_URLS
 if [ -z "${ETCD_LISTEN_CLIENT_URLS}" ]; then
@@ -13,9 +16,22 @@ if [ -z "${ETCD_LISTEN_PEER_URLS}" ]; then
 fi
 CMD="/bin/etcd -data-dir /data -listen-client-urls ${ETCD_LISTEN_CLIENT_URLS} "
 
-SERVICE_CONTAINERS=$(drill tasks.$SERVICE_NAME | grep $SERVICE_NAME | tail +2 | cut -f 5)
+getServiceContainers() {
+    SERVICE_CONTAINERS=$(drill tasks.$SERVICE_NAME | grep $SERVICE_NAME | tail +2 | cut -f 5)
+    NUM_OF_PEERS=$(echo "$SERVICE_CONTAINERS" | wc -l)
+}
+
 MY_SERVICE_IP=$(grep $(hostname) /etc/hosts | cut -f1)
-NUM_OF_PEERS=$(echo "$SERVICE_CONTAINERS" | wc -l)
+getServiceContainers
+
+while [ $NUM_OF_PEERS -lt $CLUSTER_SIZE ]; do
+    echo "Waiting for other members to start"
+    echo "Cluster Size: ${CLUSTER_SIZE}"
+    echo "Found Peers: ${NUM_OF_PEERS}"
+    echo
+    sleep 1
+    getServiceContainers
+done
 
 # Set node name
 if [ -z "${ETCD_NAME}" ]; then
@@ -54,9 +70,24 @@ if [ $NUM_OF_PEERS -gt 1 ]; then
     CMD="$CMD -listen-peer-urls ${ETCD_LISTEN_PEER_URLS} -initial-cluster ${ETCD_INITIAL_CLUSTER} -initial-advertise-peer-urls ${ETCD_INITIAL_ADVERTISE_PEER_URLS} "
 fi
 
-echo "Starting etcd"
+# Joining an existing cluster
+if [ $NUM_OF_PEERS -gt $CLUSTER_SIZE ]; then
+    echo "Joining existing cluster"
+
+    etcdctl member add ${ETCD_NAME} http://${MY_SERVICE_IP}:2380
+    if [ $? -ne 0 ]; then
+        echo "Error adding new member to cluster"
+        exit 1
+    fi
+
+    CMD="$CMD -initial-cluster-state existing "
+else
+    echo "Joining new cluster"
+fi
 
 CMD="$CMD $*"
+
+echo -e "Starting etcd\n$CMD\n"
 
 exec $CMD
 #echo $CMD
