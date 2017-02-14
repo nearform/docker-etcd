6ea57fda93dcab6af6cb10166683709a94660811
Lee Keitel <lfkeitel@usi.edu>
2017-02-14 11:39:31 -0600

    Auto-detect service name, better error message if cluster is unstable

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Docker Etcd
 
-This image weighs in at 34.6 MB due to the inclusion of TLS support and etcdctl.  The `-data-dir` is a volume mounted to `/data`, and the default ports are bound to Etcd and exposed.
+This image is based on Alpine Linux 3.5. The `-data-dir` is a volume mounted to `/data`, and the default ports are bound to Etcd and exposed. This image DOES NOT expose the old, deprecated etcd ports. It only exposes ports 2379 and 2380.
 
 ## Environment Variables
 
 These settings may be overwritten by defining the variables at run time or passing them as CLI flags to the container. CLI flags override any environment variables with the same name.
 
-- `SERVICE_NAME` - The service name when using a Swarm cluster.
+- `SERVICE_NAME` - The service name when using a Swarm cluster. Will be auto-detected if not given.
+- `CLUSTER_SIZE` - The initial size of a cluster. Defaults to 1.
 - `ETCD_NAME` - This will be unique when running as a service on Swarm, defaults to `etcd` when running as a standalone container.
 - `ETCD_LISTEN_CLIENT_URLS` - Defaults to `http://0.0.0.0:2379`.
 - `ETCD_ADVERTISE_CLIENT_URLS` -  Defaults to `http://$IP_OF_CONTAINER:2379`, manually define this if running as a single container.
@@ -16,13 +17,34 @@ These settings may be overwritten by defining the variables at run time or passi
 
 ## Using the image
 
-Arguments to etcd may be passed at run time:
+### Single node "cluster"
 
 ```sh
 docker run \
   -d \
   -p 2379:2379 \
+  -e 'ETCD_ADVERTISE_CLIENT_URLS=http://192.168.1.99:2379' \
   --name some-etcd \
-  lfkeitel/etcd:latest \
-  --advertise-client-urls http://192.168.1.99:2379
+  lfkeitel/etcd:latest
 ```
+
+### Docker Swarm
+
+When using this image as a cluster, you MUST give the environment variable `CLUSTER_SIZE` so the containers can cluster together correctly:
+
+```sh
+docker service create \
+  --name etcd \
+  -e 'CLUSTER_SIZE=3' \
+  -e 'ETCD_ADVERTISE_CLIENT_URLS=http://192.168.56.101:2379,http://192.168.56.102:2379' \
+  --replicas=3 \
+  --publish 2379:2379
+  --network=etcd \
+  lfkeitel/etcd:latest
+```
+
+## Scaling Up Swarm Service
+
+This image can handle new containers being added to the Swarm service. The container will run etcdctl to add itself as a new member then attempt to run etcd.
+
+**WARNING**: Only scale up the service ONE AT A TIME. If the service is scaled up more than one at a time, the containers may not join the cluster correctly. Then you're stuck with having to rebuild the cluster unless you're lucky enough to scale down and it kills one of the problematic containers. This is being worked on. You have been warned.
--- run.sh
+++ run.sh
@@ -1,9 +1,53 @@
 #!/bin/sh
 
-SERVICE_NAME=${SERVICE_NAME:-etcd}
+SERVICE_NAME=${SERVICE_NAME:-}
 CLUSTER_SIZE=${CLUSTER_SIZE:-1}
-SERVICE_CONTAINER=""
+SERVICE_CONTAINERS=""
 NUM_OF_PEERS=0
+CMD="/bin/etcd -data-dir /data"
+
+# Get this container's IP address
+MY_SERVICE_IP=$(grep $(hostname) /etc/hosts | cut -f1)
+if [ -z "$MY_SERVICE_IP" ]; then
+    echo "etcd must be connected to a network"
+    exit 1
+fi
+
+# Detect the service name from DNS
+getServiceName() {
+    SERVICE_NAME="$(drill -x $MY_SERVICE_IP | grep PTR | tail +2 | cut -f5 | cut -d'.' -f1)"
+}
+
+# Just in case the DNS is a tad slow, also ran once if SERVICE_NAME wasn't given
+while [ -z "$SERVICE_NAME" ]; do
+    echo "Getting service name"
+    sleep 1
+    getServiceName
+done
+
+# Get a list of the containers that are a part of this service
+getServiceContainers() {
+    SERVICE_CONTAINERS=$(drill tasks.$SERVICE_NAME | grep $SERVICE_NAME | tail +2 | cut -f5)
+    NUM_OF_PEERS=$(echo "$SERVICE_CONTAINERS" | wc -l)
+}
+getServiceContainers
+
+# Wait for all initial cluster nodes to start and added to DNS
+while [ $NUM_OF_PEERS -lt $CLUSTER_SIZE ]; do
+    echo "Waiting for other members to start"
+    echo "Cluster Size: ${CLUSTER_SIZE}"
+    echo "Found Peers: ${NUM_OF_PEERS}"
+    echo
+    sleep 1
+    getServiceContainers
+done
+
+# If a cluster already exists, wait for this containers IP to be added to DNS
+while [ -z "$(drill -x $MY_SERVICE_IP | grep $SERVICE_NAME)" ]; do
+    echo "Waiting to be added to DNS"
+    sleep 1
+    getServiceContainers
+done
 
 # Check for $ETCD_LISTEN_CLIENT_URLS
 if [ -z "${ETCD_LISTEN_CLIENT_URLS}" ]; then
@@ -14,30 +58,7 @@ fi
 if [ -z "${ETCD_LISTEN_PEER_URLS}" ]; then
         ETCD_LISTEN_PEER_URLS="http://0.0.0.0:2380"
 fi
-CMD="/bin/etcd -data-dir /data -listen-client-urls ${ETCD_LISTEN_CLIENT_URLS}"
-
-getServiceContainers() {
-    SERVICE_CONTAINERS=$(drill tasks.$SERVICE_NAME | grep $SERVICE_NAME | tail +2 | cut -f 5)
-    NUM_OF_PEERS=$(echo "$SERVICE_CONTAINERS" | wc -l)
-}
-
-MY_SERVICE_IP=$(grep $(hostname) /etc/hosts | cut -f1)
-getServiceContainers
-
-while [ $NUM_OF_PEERS -lt $CLUSTER_SIZE ]; do
-    echo "Waiting for other members to start"
-    echo "Cluster Size: ${CLUSTER_SIZE}"
-    echo "Found Peers: ${NUM_OF_PEERS}"
-    echo
-    sleep 1
-    getServiceContainers
-done
-
-while [ -z "$(drill -x $MY_SERVICE_IP | grep $SERVICE_NAME)" ]; do
-    echo "Waiting to be added to DNS"
-    sleep 1
-    getServiceContainers
-done
+CMD="$CMD -listen-client-urls ${ETCD_LISTEN_CLIENT_URLS}"
 
 # Set node name
 if [ -z "${ETCD_NAME}" ]; then
@@ -87,8 +108,17 @@ if [ $NUM_OF_PEERS -gt $CLUSTER_SIZE ]; then
     ENDPOINTS="${ENDPOINTS%?}"
 
     export ETCDCTL_API=3
-    etcdctl --endpoints="${ENDPOINTS}" member add ${ETCD_NAME} --peer-urls="http://${MY_SERVICE_IP}:2380"
-    if [ $? -ne 0 ]; then
+    etcdctl_out=$(etcdctl --endpoints="${ENDPOINTS}" member add ${ETCD_NAME} --peer-urls="http://${MY_SERVICE_IP}:2380")
+    etcdctl_exit_code=$?
+
+    # Check if multiple members are attempting to join
+    if [ -n "$(echo "$etcdctl_out" | grep 'unhealthy cluster')" ]; then
+        echo "Cluster can't accept new members right now"
+        exit 1
+    fi
+
+    # Check for other errors
+    if [ $etcdctl_exit_code -ne 0 ]; then
         echo "Error adding new member to cluster"
         exit 1
     fi
@@ -103,4 +133,3 @@ CMD="$CMD $*"
 echo -e "Starting etcd\n$CMD\n"
 
 exec $CMD
-#echo $CMD
